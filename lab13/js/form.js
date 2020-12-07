7a7facff1112cd629b560c5a972630f0db714951
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-12-06 21:22:14 -0500

    added javascript function to add event handler, and style on title of form page

---
+++ lab13/js/form.js
@@ -0,0 +1,14 @@
+function titleChange() {
+    let title = document.querySelector("h2");
+    console.log(title)
+    let flag = true;
+    if(flag) {
+        title.innerHTML = "Soccer Form (MouseOver Effect)"
+        title.style.color = "Red"
+    }
+}
+
+
+function formValidation () {
+    alert("HERE")
+}
